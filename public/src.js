163336ce9ad4a215147e41d1ba22cbc8d7348a43
Alexander <alexanderVenchik@gmail.com>
2021-11-25 19:58:18 +0500

    add axios

--- public/src.js
+++ public/src.js
@@ -1,20 +1,31 @@
-let persons = [
-    "Sasha",
-    "Masha",
-    "Каша"
-]
+const PORT = 3000
 
 let person = ""
 
 $(document).ready(() => {
-    persons.forEach(iter => {
-        $('#input').append($('<option>', {
-            value: {
+    let responseData
 
-            },
-            text: iter
-        }));
-    })
+    axios.get(`http://localhost:${PORT}/test`)
+        .then(response => {
+            if(response.status === 200)
+            {
+                // Массив объетов
+                responseData = response.data
+
+                // Заполняем comboBox
+                responseData.forEach(iter => {
+                    $('#input').append($('<option>', {
+                        value: iter.status,
+                        text: iter.author
+                    }));
+                })
+                console.log("GOOD")
+            }
+        })
+        .catch(error => {
+            console.log(error)
+        })
+
 
     $("#input").on("input", () => {
         const input = $("#input")
